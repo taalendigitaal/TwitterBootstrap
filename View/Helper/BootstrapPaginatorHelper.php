8696f2ba0a03a9aef2ecaee455bde36bddac6b52
Hendri Smit <hen3smit@gmail.com>
2014-01-21 17:30:13 +0100

    updated paginator so it can hold link for current item

--- View/Helper/BootstrapPaginatorHelper.php
+++ View/Helper/BootstrapPaginatorHelper.php
@@ -7,7 +7,7 @@ class BootstrapPaginatorHelper extends PaginatorHelper {
 		$default = array(
 			'div' => 'pagination pagination-centered'
 		);
-		
+
 		$model = (empty($options['model'])) ? $this->defaultModel() : $options['model'];
 
 		$pagingParams = $this->request->params['paging'][$model];
@@ -119,22 +119,122 @@ class BootstrapPaginatorHelper extends PaginatorHelper {
 	}
 
 	public function numbers($options = array()) {
+            if ($options === true) {
+			$options = array(
+				'before' => ' | ', 'after' => ' | ', 'first' => 'first', 'last' => 'last'
+			);
+		}
+
 		$defaults = array(
-			'tag' => 'li',
-			'before' => null,
-			'after' => null,
-			'model' => $this->defaultModel(),
-			'class' => null,
-			'modulus' => 11,
-			'separator' => false,
-			'first' => null,
-			'last' => null,
-			'ellipsis' => '<li class="disabled"><a href="#">…</a></li>',
-			'currentClass' => 'current'
+			'tag' => 'li', 'before' => null, 'after' => null, 'model' => $this->defaultModel(), 'class' => null,
+			'modulus' => '11', 'separator' => false, 'first' => null, 'last' => null, 'ellipsis' => '<li class="disabled"><a href="#">…</a></li>',
+			'currentClass' => 'active', 'currentTag' => 'a'
 		);
 		$options += $defaults;
-		$return = parent::numbers($options);
-		return preg_replace('@<li class="current">(.*?)</li>@', '<li class="current disabled"><a href="#">\1</a></li>', $return);
+
+		$params = (array)$this->params($options['model']) + array('page' => 1);
+		unset($options['model']);
+
+		if ($params['pageCount'] <= 1) {
+			return false;
+		}
+
+		extract($options);
+		unset($options['tag'], $options['before'], $options['after'], $options['model'],
+			$options['modulus'], $options['separator'], $options['first'], $options['last'],
+			$options['ellipsis'], $options['class'], $options['currentClass'], $options['currentTag']
+		);
+
+		$out = '';
+
+		if ($modulus && $params['pageCount'] > $modulus) {
+			$half = intval($modulus / 2);
+			$end = $params['page'] + $half;
+
+			if ($end > $params['pageCount']) {
+				$end = $params['pageCount'];
+			}
+			$start = $params['page'] - ($modulus - ($end - $params['page']));
+			if ($start <= 1) {
+				$start = 1;
+				$end = $params['page'] + ($modulus - $params['page']) + 1;
+			}
+
+			if ($first && $start > 1) {
+				$offset = ($start <= (int)$first) ? $start - 1 : $first;
+				if ($offset < $start - 1) {
+					$out .= $this->first($offset, compact('tag', 'separator', 'ellipsis', 'class'));
+				} else {
+					$out .= $this->first($offset, compact('tag', 'separator', 'class', 'ellipsis') + array('after' => $separator));
+				}
+			}
+
+			$out .= $before;
+
+			for ($i = $start; $i < $params['page']; $i++) {
+				$out .= $this->Html->tag($tag, $this->link($i, array('page' => $i), $options), compact('class')) . $separator;
+			}
+
+			if ($class) {
+				$currentClass .= ' ' . $class;
+			}
+
+                        if ($currentTag && $currentTag === 'a') {
+                            $out .= $this->Html->tag($tag, $this->link($params['page'], array('page' => $params['page']), $options), array('class' => $currentClass));
+                        } elseif ($currentTag) {
+				$out .= $this->Html->tag($tag, $this->Html->tag($currentTag, $params['page']), array('class' => $currentClass));
+			} else {
+				$out .= $this->Html->tag($tag, $params['page'], array('class' => $currentClass));
+			}
+			if ($i != $params['pageCount']) {
+				$out .= $separator;
+			}
+
+			$start = $params['page'] + 1;
+			for ($i = $start; $i < $end; $i++) {
+				$out .= $this->Html->tag($tag, $this->link($i, array('page' => $i), $options), compact('class')) . $separator;
+			}
+
+			if ($end != $params['page']) {
+				$out .= $this->Html->tag($tag, $this->link($i, array('page' => $end), $options), compact('class'));
+			}
+
+			$out .= $after;
+
+			if ($last && $end < $params['pageCount']) {
+				$offset = ($params['pageCount'] < $end + (int)$last) ? $params['pageCount'] - $end : $last;
+				if ($offset <= $last && $params['pageCount'] - $end > $offset) {
+					$out .= $this->last($offset, compact('tag', 'separator', 'ellipsis', 'class'));
+				} else {
+					$out .= $this->last($offset, compact('tag', 'separator', 'class', 'ellipsis') + array('before' => $separator));
+				}
+			}
+
+		} else {
+			$out .= $before;
+
+			for ($i = 1; $i <= $params['pageCount']; $i++) {
+				if ($i == $params['page']) {
+					if ($class) {
+						$currentClass .= ' ' . $class;
+					}
+					if ($currentTag) {
+						$out .= $this->Html->tag($tag, $this->Html->tag($currentTag, $i), array('class' => $currentClass));
+					} else {
+						$out .= $this->Html->tag($tag, $i, array('class' => $currentClass));
+					}
+				} else {
+					$out .= $this->Html->tag($tag, $this->link($i, array('page' => $i), $options), compact('class'));
+				}
+				if ($i != $params['pageCount']) {
+					$out .= $separator;
+				}
+			}
+
+			$out .= $after;
+		}
+
+		return $out;
 	}
 
 	public function first($title = null, $options = array()) {
